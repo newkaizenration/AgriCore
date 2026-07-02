24aea315b9f129bd3112ae80a261c49df3893960
kaizenration <kaizenration@users.noreply.github.com>
2026-07-01 19:07:05 -0500

    Fix Android build SDK mismatch by setting compileSdk and overriding compileSdkVersion for all subprojects to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.agricore.agricore"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,24 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val project = this
+    fun configureAndroid() {
+        val androidExtension = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        if (androidExtension != null) {
+            androidExtension.compileSdkVersion(36)
+        }
+    }
+
+    if (project.state.executed) {
+        configureAndroid()
+    } else {
+        project.afterEvaluate {
+            configureAndroid()
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
